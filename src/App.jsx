33deaab365bba5af88f194a8d3d72b9162761237
André <andre.bernet@stud.kbw.ch>
2025-11-20 09:47:59 +0100

    Erstelle die HaschRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <HashRouter>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/home">HomePage</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/help">Help</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/help" element={<Help />} />
+            </Routes>
+        </HashRouter>
+    );
 }
 
-export default App
+export default App;
+
+
+
+
---
+++ src/HomePage.jsx
@@ -0,0 +1,22 @@
+
+
+
+export default function(){
+
+}
+
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
